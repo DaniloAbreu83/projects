16e42c47bcbe8434c9787a2311104b417a00b5b0
Danilo Abreu <danilo13abreu@gmail.com>
2025-12-16 21:17:32 -0300

    renderizar bandeiras do data.js

--- js/data.js
+++ js/data.js
@@ -1,42 +1,210 @@
-// data.js
-
 const countries = [
     {
-        code: "BR",
-        name: "Brasil",
-        flag: "https://flagcdn.com/w320/br.png",
-        continent: "América do Sul"
+        "code": "AG",
+        "name": "Antigua and Barbuda",
+        "flag": "https://flagcdn.com/w320/ag.png",
+        "continent": "Americas"
     },
     {
-        code: "US",
-        name: "Estados Unidos",
-        flag: "https://flagcdn.com/w320/us.png",
-        continent: "América do Norte"
+        "code": "BT",
+        "name": "Bhutan",
+        "flag": "https://flagcdn.com/w320/bt.png",
+        "continent": "Asia"
     },
     {
-        code: "JP",
-        name: "Japão",
-        flag: "https://flagcdn.com/w320/jp.png",
-        continent: "Ásia"
+        "code": "IT",
+        "name": "Italy",
+        "flag": "https://flagcdn.com/w320/it.png",
+        "continent": "Europe"
     },
     {
-        code: "FR",
-        name: "França",
-        flag: "https://flagcdn.com/w320/fr.png",
-        continent: "Europa"
+        "code": "TV",
+        "name": "Tuvalu",
+        "flag": "https://flagcdn.com/w320/tv.png",
+        "continent": "Oceania"
     },
     {
-        code: "ZA",
-        name: "África do Sul",
-        flag: "https://flagcdn.com/w320/za.png",
-        continent: "África"
+        "code": "AI",
+        "name": "Anguilla",
+        "flag": "https://flagcdn.com/w320/ai.png",
+        "continent": "Americas"
     },
     {
-        code: "AU",
-        name: "Austrália",
-        flag: "https://flagcdn.com/w320/au.png",
-        continent: "Oceania"
+        "code": "AU",
+        "name": "Australia",
+        "flag": "https://flagcdn.com/w320/au.png",
+        "continent": "Oceania"
+    },
+    {
+        "code": "BZ",
+        "name": "Belize",
+        "flag": "https://flagcdn.com/w320/bz.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "VU",
+        "name": "Vanuatu",
+        "flag": "https://flagcdn.com/w320/vu.png",
+        "continent": "Oceania"
+    },
+    {
+        "code": "BY",
+        "name": "Belarus",
+        "flag": "https://flagcdn.com/w320/by.png",
+        "continent": "Europe"
+    },
+    {
+        "code": "MU",
+        "name": "Mauritius",
+        "flag": "https://flagcdn.com/w320/mu.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "LA",
+        "name": "Laos",
+        "flag": "https://flagcdn.com/w320/la.png",
+        "continent": "Asia"
+    },
+    {
+        "code": "SN",
+        "name": "Senegal",
+        "flag": "https://flagcdn.com/w320/sn.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "TR",
+        "name": "Turkey",
+        "flag": "https://flagcdn.com/w320/tr.png",
+        "continent": "Asia"
+    },
+    {
+        "code": "BO",
+        "name": "Bolivia",
+        "flag": "https://flagcdn.com/w320/bo.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "LK",
+        "name": "Sri Lanka",
+        "flag": "https://flagcdn.com/w320/lk.png",
+        "continent": "Asia"
+    },
+    {
+        "code": "NF",
+        "name": "Norfolk Island",
+        "flag": "https://flagcdn.com/w320/nf.png",
+        "continent": "Oceania"
+    },
+    {
+        "code": "CN",
+        "name": "China",
+        "flag": "https://flagcdn.com/w320/cn.png",
+        "continent": "Asia"
+    },
+    {
+        "code": "BQ",
+        "name": "Caribbean Netherlands",
+        "flag": "https://flagcdn.com/w320/bq.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "GG",
+        "name": "Guernsey",
+        "flag": "https://flagcdn.com/w320/gg.png",
+        "continent": "Europe"
+    },
+    {
+        "code": "SD",
+        "name": "Sudan",
+        "flag": "https://flagcdn.com/w320/sd.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "YT",
+        "name": "Mayotte",
+        "flag": "https://flagcdn.com/w320/yt.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "BL",
+        "name": "Saint Barthélemy",
+        "flag": "https://flagcdn.com/w320/bl.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "VA",
+        "name": "Vatican City",
+        "flag": "https://flagcdn.com/w320/va.png",
+        "continent": "Europe"
+    },
+    {
+        "code": "TC",
+        "name": "Turks and Caicos Islands",
+        "flag": "https://flagcdn.com/w320/tc.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "CW",
+        "name": "Curaçao",
+        "flag": "https://flagcdn.com/w320/cw.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "BW",
+        "name": "Botswana",
+        "flag": "https://flagcdn.com/w320/bw.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "BJ",
+        "name": "Benin",
+        "flag": "https://flagcdn.com/w320/bj.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "LT",
+        "name": "Lithuania",
+        "flag": "https://flagcdn.com/w320/lt.png",
+        "continent": "Europe"
+    },
+    {
+        "code": "MS",
+        "name": "Montserrat",
+        "flag": "https://flagcdn.com/w320/ms.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "VG",
+        "name": "British Virgin Islands",
+        "flag": "https://flagcdn.com/w320/vg.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "BI",
+        "name": "Burundi",
+        "flag": "https://flagcdn.com/w320/bi.png",
+        "continent": "Africa"
+    },
+    {
+        "code": "UM",
+        "name": "United States Minor Outlying Islands",
+        "flag": "https://flagcdn.com/w320/um.png",
+        "continent": "Americas"
+    },
+    {
+        "code": "IE",
+        "name": "Ireland",
+        "flag": "https://flagcdn.com/w320/ie.png",
+        "continent": "Europe"
+    },
+    {
+        "code": "SB",
+        "name": "Solomon Islands",
+        "flag": "https://flagcdn.com/w320/sb.png",
+        "continent": "Oceania"
     }
 ];
 
+
 export { countries };
+
--- js/main.js
+++ js/main.js
@@ -1,41 +1,14 @@
+import { countries } from "./data.js";
 import { CountryCard } from "./components/CountryCard.js";
 
-console.log("✅ main.js está rodando");
+console.log("✅ main.js está rodando (dados locais)");
 
 const container = document.getElementById("flags-cards");
 const searchInput = document.getElementById("search");
 
-let countries = [];
-
-async function fetchCountries() {
-    try {
-        const response = await fetch(
-            "https://restcountries.com/v3.1/all?fields=cca2,name,flags,region"
-        );
-
-        if (!response.ok) {
-            throw new Error("Erro ao buscar países");
-        }
-
-        const data = await response.json();
-
-        console.log("dados da API:", data);
-
-        countries = data.map(country => ({
-            code: country.cca2,
-            name: country.name.common,
-            flag: country.flags.png,
-            continent: country.region || "N/A"
-        }));
-
-        renderCountries(countries);
-    } catch (error) {
-        console.error("❌ ERRO NO FETCH:", error);
-    }
-}
+renderCountries(countries);
 
 function renderCountries(list) {
-    console.log("renderizando", list.length, "países");
     container.innerHTML = list.map(CountryCard).join("");
 }
 
@@ -49,5 +22,4 @@ searchInput.addEventListener("input", () => {
     renderCountries(filtered);
 });
 
-fetchCountries();
 
